9164bb36258349a575b92326ed5f5c2d9131289a
ʟ๏ᴠᴇʀ ◜‿◝ <79841865+loverboyXD@users.noreply.github.com>
2021-08-15 12:57:11 +0530

    <\Lovish/>

--- love/pyrogramee/pluginshelper.py
+++ love/pyrogramee/pluginshelper.py
@@ -22,9 +22,9 @@ from pyrogram.errors import FloodWait, MessageNotModified
 
 from pyrogram.types import Chat, Message, User
 
-from DaisyX  import OWNER_ID, SUPPORT_CHAT
+from love  import OWNER_ID, SUPPORT_CHAT
 
-from DaisyX import pbot
+from love import pbot
 
 def get_user(message: Message, text: str) -> [int, str, None]:
 
