aa39683a8400cfb1d77b78f4ad0c0459afaf2bfb
ʟ๏ᴠᴇʀ ◜‿◝ <79841865+loverboyXD@users.noreply.github.com>
2021-08-15 13:28:14 +0530

    Update cust_filters_sql.py

--- love/modules/sql/cust_filters_sql.py
+++ love/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 
-from DaisyX.modules.helper_funcs.msg_types import Types
-from DaisyX.modules.sql import BASE, SESSION
+from love.modules.helper_funcs.msg_types import Types
+from love.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
