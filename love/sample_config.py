a87ca6617c03f5accb04d27996b9fda5540896d8
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2021-11-24 20:26:43 +0530

    Update sample_config.py

--- love/sample_config.py
+++ love/sample_config.py
@@ -17,9 +17,9 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1258905497  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Alone_loverboy"
-    SUPPORT_CHAT = "PsychoBots_Chat"  # Your own group for support, do not add the @
+    OWNER_ID = 2068551800  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Hayat_Murat_30"
+    SUPPORT_CHAT = "PikachuX_support"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001161549484
     )  # Prints any new group the bot is added to, prints just the name and ID.
